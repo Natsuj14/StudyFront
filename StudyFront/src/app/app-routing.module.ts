0d832b2a673f8c5ada5661507a8ed90b42e4c3e5
Jhon Edison Quiroga Jimenez <Natsuj14>
2023-09-05 10:28:05 -0500

    Se realizo la configuracion del routing-outlet para el direccionamiento

--- StudyFront/src/app/app-routing.module.ts
+++ StudyFront/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsuarioComponent } from './Componentes/usuario/usuario.component';
+import { RolComponent } from './Componentes/rol/rol.component';
+import { PruebaComponent } from './Componentes/prueba/prueba.component';
+import { AreaComponent } from './Componentes/area/area.component';
+import { EstadisticaComponent } from './Componentes/estadistica/estadistica.component';
+import { IngresoComponent } from './Componentes/ingreso/ingreso.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "Usuarios", component: UsuarioComponent },
+  { path: "Roles", component: RolComponent },
+  { path: "Pruebas", component: PruebaComponent },
+  { path: "Areas", component: AreaComponent },
+  { path: "Estadisticas", component: EstadisticaComponent },
+  { path: "Ingresos", component: IngresoComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
